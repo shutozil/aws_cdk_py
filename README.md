099b6e436b1b90e2909ba8b2d078af7b041bab3f
shutoinoue <wilshere1019arsenal@gmail.com>
2020-08-17 17:19:43 +0900

    handling context

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # aws_cdk_py
+
+## How to start server
+```
+$ docker-compose build
+$ docker-compose up -d
+$ docker exec -it ContainerID /bin/bash
+```
+
+## Activating the Virtualenv
+-  To activate your virtualenv on a Linux or MacOs platform:
+```
+$ source .env/bin/activate
+```
+- install the required python modules.
+```
+$ pip install --upgrade pip && pip install -r requirements.txt
+```
+
+## Learn Project Structure
+- https://cdkworkshop.com/30-python/20-create-project/300-structure.html
+
+## Synthesize a template from app
+- This will output the following CloudFormation template:
+```
+$ cdk [--profile PROFILE] synth
+```
+
+## Command List (ja-blog)
+- https://dev.classmethod.jp/articles/aws-cdk-command-line-interface/
+
+## Bootstrapping an environment
+- The first time you deploy an AWS CDK app into an environment (account/region)
+- need to install a bootstrap stack
+```
+$ cdk [--profile PROFILE] bootstrap
+```
+
+## How to deploy
+```
+$ cdk [--profile PROFILE] deploy STACK_NAME --context env=ENV
+```
+
+## Handling Context at cdk.json
+- https://docs.aws.amazon.com/cdk/latest/guide/get_context_var.html
+- https://dev.classmethod.jp/articles/aws-cdk-command-line-interface/#toc-14
+  - Context 値は AWS CDK が自動で生成して使い回すものもあるし、開発者が明示的に保管しておく値もある
+
+
+## Check Software Version
+```
+$ aws --version
+$ cdk --version
+```
+
+## Check Profile
+- https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/cli-configure-files.html
+```
+$ cat ~/.aws/credentials
+$ cat ~/.aws/config
+```
--- cdk/cdk/test_cdk_project_stack.py
+++ cdk/cdk/test_cdk_project_stack.py
@@ -8,38 +8,41 @@ class TestCdkProjectStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+        env = self.node.try_get_context("env")    # 明示的にcommandで渡すべきcontext
+        context = self.node.try_get_context(env)    # 明示的に渡したcontextから自動で取得するcontext
+        cidr = context["cidr_ip"]
 
-        cidr = '24.102.0.0/16'
-
+        vpc_id = env + "-vpc"
         vpc = aws_ec2.Vpc(
             self,
-            id='test-vpc',
+            id=vpc_id,
             cidr=cidr,
             nat_gateways=1,
             subnet_configuration=[
                 aws_ec2.SubnetConfiguration(
-                    cidr_mask=18,
+                    cidr_mask=20,
                     name='public',
                     subnet_type=aws_ec2.SubnetType.PUBLIC,
                 ),
                 aws_ec2.SubnetConfiguration(
-                    cidr_mask=18,
+                    cidr_mask=20,
                     name='private',
                     subnet_type=aws_ec2.SubnetType.PRIVATE,
                 ),
                 aws_ec2.SubnetConfiguration(
-                    cidr_mask=24,
+                    cidr_mask=20,
                     name='protected',
                     subnet_type=aws_ec2.SubnetType.PUBLIC,
                 )
             ],
         )
 
+        sg_name = env + "-security-group"
         security_group = aws_ec2.SecurityGroup(
             self,
-            id='test-security-group',
+            id=sg_name,
             vpc=vpc,
-            security_group_name='test-security-group'
+            security_group_name=sg_name
         )
 
         security_group.add_ingress_rule(
@@ -49,17 +52,18 @@ class TestCdkProjectStack(core.Stack):
 
         image_id = aws_ec2.AmazonLinuxImage(generation=aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2).get_image(self).image_id
 
+        instance_name = env + "-instance"
         aws_ec2.CfnInstance(
             self,
-            id='test-instance',
+            id=instance_name,
             availability_zone="ap-northeast-1a",
             image_id=image_id,
-            instance_type="t2.micro",
+            instance_type=context["instance_type"],
             key_name='cdk-test-ec2-key',
             security_group_ids=[security_group.security_group_id],
             subnet_id=vpc.private_subnets[0].subnet_id,
             tags=[{
                 "key": "Name",
-                "value": "test-instance"
+                "value": instance_name
             }]
         )
